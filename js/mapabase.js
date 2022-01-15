6451633f1149521c12d5236e602c639e8fb119b5
rcasanovalara <rcasanovalara@gmail.com>
2022-01-15 16:06:29 +0100

    mapa base leaflet

--- js/mapabase.js
+++ js/mapabase.js
@@ -27,7 +27,7 @@ function init() {
         }).addTo(hibrid);
 
     var Stamen_TonerHybrid = L.tileLayer(
-        'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-hybrid/{z}/{x}/{y}{r}.{ext}', {
+        'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-hybrid/{z}/{x}/{y}{r}.{ext}', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         subdomains: 'abcd',
         minZoom: 0,
